fea52d13c4c857773280dfd56b812c3acf56ccb2
Sydney Renee <sydney@solace.ofharmony.ai>
2025-01-15 09:35:52 -0800

    Starting a new branch

--- ncps/tf/cfc.py
+++ ncps/tf/cfc.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import ncps
 from . import CfCCell, MixedMemoryRNN, WiredCfCCell
 import tensorflow as tf
